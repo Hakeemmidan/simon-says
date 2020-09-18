2ecda0ebabbabb915ea20229ea198bf197766d70
Hakeem Almidan <hakeemmidan@gmail.com>
2020-09-18 17:30:32 +0300

    Install + setup 'react-toastify' package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {ToastContainer} from 'react-toastify';
 import {GameContainer} from './components/GameContainer';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
+      <ToastContainer />
       <GameContainer />
     </div>
   );
